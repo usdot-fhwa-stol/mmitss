6327680b1570f66d76dbe0a7f28b652b79316d11
nvaltekar <samuelcornejo@arizona.edu>
2023-11-10 15:27:58 -0700

    miau

--- test/vsp/testSRM/test_SRM.py
+++ test/vsp/testSRM/test_SRM.py
@@ -75,6 +75,67 @@ def test_countSRM():
     #evaluate if number of SRMs is the same as number of BSMs in map
     assert(bsmsInMap == SRMreceived)
 
+def test_countNoSRM():
+     #set bsmReceiver in the same port that the MMITSS-ROS2 Receiver sends the json formatted hostBSM 
+
+    configFile = open("/nojournal/bin/mmitss-phase3-master-config.json", 'r')
+    config = (json.load(configFile))
+    configFile.close()
+
+    # initialize sockets for MAP and BSM broadcasting
+
+    hostIp = config["HostIp"]
+    portBSM = config["PortNumber"]["PriorityRequestGenerator"]
+    socketBSM = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socketBSM.bind((hostIp,portBSM))
+    portMAP = config["PortNumber"]["MessageTransceiver"]["MessageDecoder"]
+    socketMAP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socketMAP.bind((hostIp,portMAP))
+    communicationInfo = (hostIp, portBSM)
+    
+    # initialize sockets for SRM receiving
+    portSRM = config["PortNumber"]["MessageTransceiver"]["MessageEncoder"]
+    socketSRM = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socketSRM.bind((hostIp,portSRM))
+    
+    #Obtain messages
+
+    fileName = "bsm.json"
+    f = open(fileName,"r")
+    dataBSM = f.read()
+    dataBSM["BasicVehicle"]["position"]["latitude_DecimalDegree"]=40
+    dataBSM=dataBSM.encode()
+    f.close()
+    fileName = "map.json"
+    f = open(fileName,"r")
+    dataMAP = f.read().encode()
+    f.close()
+    
+    numberMAPs = 2
+    bsmSendingTime = 0.0
+    MAPSendingTime = 0.0
+    bsmsInMap = 0
+    SRMreceived = 0
+
+    for i in range(numberMAPs):
+        #broadcast map and bsm messages
+        if time.time()-bsmSendingTime >= 0.1:
+            socketBSM.sendto(dataBSM,communicationInfo)
+            bsmSendingTime = time.time()
+            bsmsInMap += 1 
+        if time.time()-MAPSendingTime >= 1:
+            socketMAP.sendto(dataMAP,communicationInfo)
+            MAPSendingTime = time.time()
+        #receive generated SRMs 
+        try:
+            dataSRM, address = socketSRM.recvfrom(10240)
+            dataSRM = dataSRM.decode()
+            receivedMessage = json.loads(dataSRM)
+            SRMreceived += 1 
+    
+    #evaluate if number of SRMs is the same as number of BSMs in map
+    assert(0 == SRMreceived)
+
 def test_countSRMROS2():
 
     #set bsmReceiver in the same port that the MMITSS-ROS2 Receiver sends the json formatted hostBSM 
@@ -153,3 +214,4 @@ def test_countSRMROS2():
 
     node.destroy_node()
     rclpy.shutdown()
+
