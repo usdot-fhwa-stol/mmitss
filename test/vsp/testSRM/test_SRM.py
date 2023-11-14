95612d1f8bd8aba71d00cbe16b0abf60b2a29acb
nvaltekar <samuelcornejo@arizona.edu>
2023-11-13 17:54:37 -0700

    finished SRM test

--- test/vsp/testSRM/test_SRM.py
+++ test/vsp/testSRM/test_SRM.py
@@ -15,6 +15,31 @@ import json
 import datetime
 import time
 
+ #create a subscriber class that will store the accumulate the number of SRM messages received
+class Subscriber:
+    def __init__(self):
+        self.SRMreceived = 0
+        self.node = rclpy.create_node("my_subscriber")
+        self.subscriber = self.node.create_subscription(
+            ByteArray,
+            "/hardware_interface/comms/outbound_binary_message",
+            self.callback,
+            10  # Set the queue size
+        )
+        
+    def callback(self, msg):
+            # Callback function to process received messages
+            self.SRMreceived +=1
+
+    def spin(self):
+        # Spin the node to start processing callbacks
+        rclpy.spin_once(self.node)
+
+    def destroy_node(self):
+        # Cleanup when the node is shutting down
+        self.node.destroy_node()
+
+
 
 def test_countSRM():
    
@@ -160,41 +185,19 @@ def test_countNoSRM():
                     pass
     #evaluate if number of SRMs is the same as number of BSMs in map
     assert(0 == SRMreceived)
-# test_countNoSRM()
+
+
 def test_countSRMROS2():
-
-    #create a subscriber class that will store the accumulate the number of SRM messages received
-    class Subscriber:
-        def __init__(self):
-            self.SRMreceived = 0
-            self.node = rclpy.create_node("my_subscriber")
-            self.subscriber = self.node.create_subscription(
-                ByteArray,
-                "/hardware_interface/comms/outbound_binary_message",
-                self.callback,
-                10  # Set the queue size
-            )
-
-        def callback(self, msg):
-                # Callback function to process received messages
-                self.SRMreceived +=1
-
-        def spin(self):
-            # Spin the node to start processing callbacks
-            rclpy.spin_once(self.node)
-
-        def destroy_node(self):
-            # Cleanup when the node is shutting down
-            self.node.destroy_node()
-
-
+    rclpy.init()
+    
+    subscriber = Subscriber()
+    
     #set bsmReceiver in the same port that the MMITSS-ROS2 Receiver sends the json formatted hostBSM 
-
     configFile = open("/nojournal/bin/mmitss-phase3-master-config.json", 'r')
     config = (json.load(configFile))
     configFile.close()
 
-   # initialize sockets for MAP and BSM broadcasting
+    # initialize sockets for MAP and BSM broadcasting
     hostIp = config["HostIp"]
     portBSM = config["PortNumber"]["HostBsmDecoder"]
     socketBSM = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -224,17 +227,11 @@ def test_countSRMROS2():
     SRMreceived = 0
 
 
-
-
     #create ROS2 node subscribing to SRM published topic
 
-    try:
-        rclpy.init()
-    except:
-        pass
-    subscriber = Subscriber()
-    
-   
+
+    #receive generated SRMs on ROS2 
+    SRMreceived = 0
     for i in range(numberMAPs):
         #broadcast map and bsm messages
         if time.time()-MAPSendingTime >= 1:
@@ -244,16 +241,15 @@ def test_countSRMROS2():
         if time.time()-bsmSendingTime >= 0.1:
             socketBSM.sendto(dataBSM,communicationInfo)
             bsmSendingTime = time.time()
-            bsmsInMap += 1 
-        
-    #receive generated SRMs on ROS2 
-    
-    subscriber.spin()
+            bsmsInMap += 1
+            time.sleep(3) 
+            subscriber.spin()
     SRMreceived = subscriber.SRMreceived
     subscriber.destroy_node()
-    rclpy.shutdown()            
-    assert(bsmsInMap == subscriber.SRMreceived)
+    rclpy.shutdown()
+    #receive generated SRMs on ROS2 
+    
+    #evaluate if number of SRMs is the same as number of BSMs in map
+    assert(bsmsInMap == SRMreceived)
 
-        #evaluate if number of SRMs is the same as number of BSMs in map
-        
 
