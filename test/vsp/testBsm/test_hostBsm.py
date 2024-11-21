a3074a77a7e76879218eeab49eec48310ef92093
Dylan-Wyl10 <dylanwyl10@gmail.com>
2024-11-21 11:00:01 -0500

    update for mmitss-ros2 deployment

--- test/vsp/testBsm/test_hostBsm.py
+++ test/vsp/testBsm/test_hostBsm.py
@@ -1,3 +1,5 @@
+ #!/usr/bin/env python3
+
 import subprocess
 import threading
 from carma_driver_msgs.msg import ByteArray
@@ -31,7 +33,7 @@ def test_countBsm():
    
     #set bsmReceiver in the same port that the MMITSS-ROS2 Receiver sends the json formatted hostBSM 
 
-    configFile = open("/nojournal/bin/mmitss-phase3-master-config.json", 'r')
+    configFile = open("nojournal/bin/mmitss-phase3-master-config.json", 'r')
     config = (json.load(configFile))
     configFile.close()
 
@@ -75,7 +77,12 @@ def test_countBsm():
                 
                 except:
                     pass
+    print(numMsgs)
+    print(hostBSMReceived)
 
     assert(numMsgs == hostBSMReceived)
+    
+# test_countBsm()
+test_listenToRos()
 
 
