79f34c43a7028db253706a192bc49074e641661b
Dylan-Wyl10 <dylanwyl10@gmail.com>
2024-12-02 11:08:05 -0500

    update files

--- test/vsp/testBsm/runTest.sh
+++ test/vsp/testBsm/runTest.sh
@@ -5,4 +5,4 @@ source /opt/ros/foxy/setup.bash
 source /home/yilin/SIP_project/carma-msgs/install/setup.bash 
 source ../../../mmitss/src/common/MsgTransceiver/CarmaDecoderRos2/install/setup.bash
 
-../../../mmitss/src/common/MsgTransceiver/CarmaDecoderRos2/src/M_CarmaMsgDecoder & sleep 2;python3 -m test_hostBsm.py
+../../../mmitss/src/common/MsgTransceiver/CarmaDecoderRos2/src/M_CarmaMsgDecoder & sleep 2;python3 -m unittest test_hostBsm.py
