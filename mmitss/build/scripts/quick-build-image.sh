8a3b1f2b25f758a34c159b7bad33a4d8ff6d7287
Dylan-Wyl10 <dylanwyl10@gmail.com>
2025-01-28 11:35:40 -0500

    clear the duplication

--- mmitss/build/scripts/quick-build-image.sh
+++
@@ -1,4 +0,0 @@
-# a temporal script to quickly build docker image after all component has been built.
-
-cd ../..
-docker build -t mmitssuarizona/mmitss-carma-vsp-ros2-x86:CDA1.0 -f build/dockerfiles/x86/Dockerfile.carma_vsp_ros2 .
