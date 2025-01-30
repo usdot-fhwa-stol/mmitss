af4f4747f7161fca969fa0201f11cabf5e8a1873
Dylan-Wyl10 <dylanwyl10@gmail.com>
2025-01-30 10:34:05 -0500

    fix the ros2 issue

--- mmitss/build/dockerfiles/x86/build_vsp_ros2_transceivers.sh
+++ mmitss/build/dockerfiles/x86/build_vsp_ros2_transceivers.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+set -e
+cd /mmitss/tmp/mmitss/src/common
+build clean && build linux
+
 source /opt/ros/foxy/setup.bash
 cd /mmitss/tmp/mmitss/src/common/MsgTransceiver/CarmaDecoderRos2
+[ -d "build" ] && rm -rf "build"
+[ -d "install" ] && rm -rf "install"
+[ -d "log" ] && rm -rf "log"
 source /root/dev_ws/src/carma-msgs/install/setup.bash && colcon build
+[ -d "build" ] && rm -rf "build"
+[ -d "install" ] && rm -rf "install"
+[ -d "log" ] && rm -rf "log"
 cd /mmitss/tmp/mmitss/src/common/MsgTransceiver/CarmaEncoderRos2
 source /root/dev_ws/src/carma-msgs/install/setup.bash && colcon build
--- mmitss/build/dockerfiles/x86/install_carma_msgs.sh
+++ mmitss/build/dockerfiles/x86/install_carma_msgs.sh
@@ -1,6 +1,8 @@
-# install ros2
+#! /bin/bash
+set -e
+# install ros
 
-apt update && sudo apt install locales
+apt update && apt install locales
 locale-gen en_US en_US.UTF-8
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
@@ -11,25 +13,19 @@ curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /us
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 apt update
-DEBIAN_FRONTEND=noninteractive apt install -y ros-foxy-desktop
-apt-get -y update
-apt-get -y install git
+DEBIAN_FRONTEND=noninteractive apt install -y ros-foxy-desktop git python3-rosdep python3-colcon-common-extensions ros-foxy-rmw-cyclonedds-cpp
 
 cd /root/dev_ws/src
 git clone https://github.com/ros/ros_tutorials.git -b foxy-devel
 cd /root/dev_ws
 
-apt-get install python3-rosdep -y
 rosdep init
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y --rosdistro foxy
 
-# install colcon
-apt install python3-colcon-common-extensions -y
-
 # install carma-msg
 cd /root/dev_ws/src
 git clone https://github.com/usdot-fhwa-stol/carma-msgs.git
-cd /carma-msgs
+cd carma-msgs
 source /opt/ros/foxy/setup.bash && colcon build --packages-up-to carma_driver_msgs
 
