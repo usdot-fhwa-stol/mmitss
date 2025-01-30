3f8bb2dc436f49341674430018bacfc0026b7747
Dylan-Wyl10 <dylanwyl10@gmail.com>
2025-01-30 12:40:39 -0500

    update

--- mmitss/build/dockerfiles/x86/build_vsp_ros2_transceivers.sh
+++ mmitss/build/dockerfiles/x86/build_vsp_ros2_transceivers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-cd /mmitss/tmp/mmitss/src/common
+cd /mmitss/tmp/src/common
 build clean && build linux
 
 source /opt/ros/foxy/setup.bash
-cd /mmitss/tmp/mmitss/src/common/MsgTransceiver/CarmaDecoderRos2
+cd /mmitss/tmp/src/common/MsgTransceiver/CarmaDecoderRos2
 [ -d "build" ] && rm -rf "build"
 [ -d "install" ] && rm -rf "install"
 [ -d "log" ] && rm -rf "log"
@@ -12,5 +12,5 @@ source /root/dev_ws/src/carma-msgs/install/setup.bash && colcon build
 [ -d "build" ] && rm -rf "build"
 [ -d "install" ] && rm -rf "install"
 [ -d "log" ] && rm -rf "log"
-cd /mmitss/tmp/mmitss/src/common/MsgTransceiver/CarmaEncoderRos2
+cd /mmitss/tmp/src/common/MsgTransceiver/CarmaEncoderRos2
 source /root/dev_ws/src/carma-msgs/install/setup.bash && colcon build
