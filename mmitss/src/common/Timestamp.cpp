7a0b0594ea7064d5def00e65a4b822260a5e4d9c
dev <dev@Focal.myguest.virtualbox.org>
2025-03-03 12:43:48 -0500

    Updates

--- mmitss/src/common/Timestamp.cpp
+++ mmitss/src/common/Timestamp.cpp
@@ -1,25 +1,40 @@
 #include <string>
 #include <sys/time.h>
+#include <udp_time_sync/TimeSync.hpp>
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include <ctime>
 
 std::string getVerboseTimestamp()
 {
-    timeval curTime;
+    auto ms =time_sync::nowInMilliseconds();
+    // Convert milliseconds to a time_point
+    std::chrono::milliseconds duration(ms);
+    auto time_point = std::chrono::system_clock::from_time_t(0) + duration;
 
-    gettimeofday(&curTime, NULL);
+    // Convert to time_t for formatting
+    std::time_t t = std::chrono::system_clock::to_time_t(time_point);
+    std::tm tm = *std::localtime(&t);
 
-    long int milli = curTime.tv_usec / 1000;
-    char buf[sizeof "2011-10-08 07:07:09.000"];
-    strftime(buf, sizeof buf, "%F %T", gmtime(&curTime.tv_sec));
-    sprintf(buf, "%s.%ld", buf, milli);
+    // Create string stream to format date and time
+    std::stringstream ss;
+    ss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
 
-    return buf;
+    // Get milliseconds
+    int milliseconds = ms % 1000;
+
+    // Append milliseconds to the time string
+    ss << "." << std::setw(3) << std::setfill('0') << milliseconds;
+
+    return ss.str();
 }  
 
 double getPosixTimestamp()
 {
 
-	struct timeval tv_tt;
-	gettimeofday(&tv_tt, NULL);
-	return (static_cast<double>(tv_tt.tv_sec)+static_cast<double>(tv_tt.tv_usec)/1.0e6);  
+	auto ms_timestamp = time_sync::nowInMilliseconds();
+	return static_cast<double>(ms_timestamp) / 1000.0;  
 
 }
