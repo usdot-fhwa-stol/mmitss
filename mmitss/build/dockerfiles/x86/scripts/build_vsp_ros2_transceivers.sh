f5c7a849cdb029dabae98be794aad65d08076ff3
dev <dev@Focal.myguest.virtualbox.org>
2025-03-28 14:27:11 -0400

    Updates

--- mmitss/build/dockerfiles/x86/scripts/build_vsp_ros2_transceivers.sh
+++ mmitss/build/dockerfiles/x86/scripts/build_vsp_ros2_transceivers.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 set -e
-
-source /opt/ros/foxy/setup.bash
 cd /mmitss/tmp/src/common/MsgTransceiver/CarmaDecoderRos2
+# Remove old build, install, and log directories
 [ -d "build" ] && rm -rf "build"
 [ -d "install" ] && rm -rf "install"
 [ -d "log" ] && rm -rf "log"
 source /root/dev_ws/src/carma-msgs/install/setup.bash && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Debug
 cp /mmitss/tmp/src/common/MsgTransceiver/CarmaDecoderRos2/build/carma-ros2-decoder/M_CarmaMsgDecoder /mmitss/
-# [ -d "build" ] && rm -rf "build"
-# [ -d "install" ] && rm -rf "install"
-# [ -d "log" ] && rm -rf "log"
 cd /mmitss/tmp/src/common/MsgTransceiver/CarmaEncoderRos2
+# Remove old build, install, and log directories
 [ -d "build" ] && rm -rf "build"
 [ -d "install" ] && rm -rf "install"
 [ -d "log" ] && rm -rf "log"
 source /root/dev_ws/src/carma-msgs/install/setup.bash && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Debug
 cp /mmitss/tmp/src/common/MsgTransceiver/CarmaEncoderRos2/build/carma-ros2-encoder/M_CarmaMsgEncoder /mmitss/
-# [ -d "build" ] && rm -rf "build"
-# [ -d "install" ] && rm -rf "install"
-# [ -d "log" ] && rm -rf "log"
 
