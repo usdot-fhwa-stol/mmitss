da4564bdbb93cc50531c551a9e0366c22e79562c
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2025-04-11 11:08:22 -0400

    Update build-mmitss-ci.sh

--- mmitss/build/scripts/build-mmitss-ci.sh
+++ mmitss/build/scripts/build-mmitss-ci.sh
@@ -23,7 +23,6 @@
 #############################################################################################
 set +e
 
-apt update 
 apt install -y python3-pip
 python3 -m pip install pyinstaller bistring apscheduler
 # Define colors:
