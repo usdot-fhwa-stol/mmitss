7935060ad551aa778161d6d1c8837824e2245d1c
dev <dev@Focal.myguest.virtualbox.org>
2025-04-18 09:48:26 -0400

    Update

--- mmitss/build/scripts/setup-build-environment-ci.sh
+++ mmitss/build/scripts/setup-build-environment-ci.sh
@@ -58,62 +58,62 @@ echo "Adding environment variable required for SNMP sessions to ~/.bashrc"
 echo "export MIBS=ALL" >> ~/.bashrc
 
 echo "Creating required directories in the root folder."
-sudo rm -r /nojournal/
-sudo rm -r /usr/local/lib/mmitss
-sudo mkdir -p /nojournal/bin/log
-sudo mkdir /usr/local/lib/mmitss
+rm -r /nojournal/
+rm -r /usr/local/lib/mmitss
+mkdir -p /nojournal/bin/log
+mkdir /usr/local/lib/mmitss
 sleep 1s
 
 if [ "$copy_sample" = "y" ]; then
 echo "Copy the configuration files of the intersection speedway-mountain to /nojournal/bin/"
-sudo cp -r ../../config/speedway-sample/simulation/speedway-mountain/nojournal/bin /nojournal
+cp -r ../../config/speedway-sample/simulation/speedway-mountain/nojournal/bin /nojournal
 sleep 1s
 echo "Change the owner and group of the configuration files and provide necessary permissions (chmod 777)"
-sudo chown -R $username:$usergroup /nojournal
-sudo chmod -R 777 /nojournal
+chown -R $username:$usergroup /nojournal
+chmod -R 777 /nojournal
 sleep 1s
 fi
 
 echo "Add the shared libraries we need to run"
 
 if [ "$arch" = "x86" ]; then
-sudo cp ../../3rdparty/net-snmp/lib/x86/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/
-sudo cp ../../3rdparty/glpk/lib/x86/libglpk.so.35.1.0 /usr/local/lib/mmitss/
-sudo cp ../../lib/x86/libmmitss-common.so /usr/local/lib/mmitss/
-sudo cp ../../3rdparty/mapengine/lib/x86/liblocAware.so.1.0 /usr/local/lib/mmitss/
-sudo cp ../../3rdparty/asn1j2735/lib/x86/libasn.so.1.0 /usr/local/lib/mmitss/
-sudo cp ../../3rdparty/asn1j2735/lib/x86/libdsrc.so.1.0 /usr/local/lib/mmitss/
-sudo cp ../../lib/mmitss.conf /etc/ld.so.conf.d/
+cp ../../3rdparty/net-snmp/lib/x86/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/
+cp ../../3rdparty/glpk/lib/x86/libglpk.so.35.1.0 /usr/local/lib/mmitss/
+cp ../../lib/x86/libmmitss-common.so /usr/local/lib/mmitss/
+cp ../../3rdparty/mapengine/lib/x86/liblocAware.so.1.0 /usr/local/lib/mmitss/
+cp ../../3rdparty/asn1j2735/lib/x86/libasn.so.1.0 /usr/local/lib/mmitss/
+cp ../../3rdparty/asn1j2735/lib/x86/libdsrc.so.1.0 /usr/local/lib/mmitss/
+cp ../../lib/mmitss.conf /etc/ld.so.conf.d/
 echo "Create the symbolic links for the copied libraries."
-sudo ln -s /usr/local/lib/mmitss/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/libnetsnmp.so.35
-sudo ln -s /usr/local/lib/mmitss/libglpk.so.35.1.0 /usr/local/lib/mmitss/libglpk.so.35
-sudo ln -s /usr/local/lib/mmitss/liblocAware.so.1.0 /usr/local/lib/mmitss/liblocAware.so
-sudo ln -s /usr/local/lib/mmitss/libasn.so.1.0 /usr/local/lib/mmitss/libasn.so
-sudo ln -s /usr/local/lib/mmitss/libdsrc.so.1.0 /usr/local/lib/mmitss/libdsrc.so
+ln -s /usr/local/lib/mmitss/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/libnetsnmp.so.35
+ln -s /usr/local/lib/mmitss/libglpk.so.35.1.0 /usr/local/lib/mmitss/libglpk.so.35
+ln -s /usr/local/lib/mmitss/liblocAware.so.1.0 /usr/local/lib/mmitss/liblocAware.so
+ln -s /usr/local/lib/mmitss/libasn.so.1.0 /usr/local/lib/mmitss/libasn.so
+ln -s /usr/local/lib/mmitss/libdsrc.so.1.0 /usr/local/lib/mmitss/libdsrc.so
 fi
 
 if [ "$arch" = "arm" ]; then
-sudo cp ../../3rdparty/net-snmp/lib/arm/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/
-sudo cp ../../3rdparty/glpk/lib/arm/libglpk.so.40.3.0 /usr/local/lib/mmitss/
-sudo cp ../../lib/arm/libmmitss-common.so /usr/local/lib/mmitss/
-sudo cp ../../3rdparty/mapengine/lib/arm/liblocAware.so.1.0 /usr/local/lib/mmitss/
-sudo cp ../../3rdparty/asn1j2735/lib/arm/libasn.so.1.0 /usr/local/lib/mmitss/
-sudo cp ../../3rdparty/asn1j2735/lib/arm/libdsrc.so.1.0 /usr/local/lib/mmitss/
-sudo cp ../../lib/mmitss.conf /etc/ld.so.conf.d/
-sudo cp ../../3rdparty/openssl/* /usr/local/lib
+cp ../../3rdparty/net-snmp/lib/arm/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/
+cp ../../3rdparty/glpk/lib/arm/libglpk.so.40.3.0 /usr/local/lib/mmitss/
+cp ../../lib/arm/libmmitss-common.so /usr/local/lib/mmitss/
+cp ../../3rdparty/mapengine/lib/arm/liblocAware.so.1.0 /usr/local/lib/mmitss/
+cp ../../3rdparty/asn1j2735/lib/arm/libasn.so.1.0 /usr/local/lib/mmitss/
+cp ../../3rdparty/asn1j2735/lib/arm/libdsrc.so.1.0 /usr/local/lib/mmitss/
+cp ../../lib/mmitss.conf /etc/ld.so.conf.d/
+cp ../../3rdparty/openssl/* /usr/local/lib
 
 
 echo "Create the symbolic links for the copied libraries."
-sudo ln -s /usr/local/lib/mmitss/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/libnetsnmp.so.35
-sudo ln -s /usr/local/lib/mmitss/libglpk.so.40.3.0 /usr/local/lib/mmitss/libglpk.so.40
-sudo ln -s /usr/local/lib/mmitss/liblocAware.so.1.0 /usr/local/lib/mmitss/liblocAware.so
-sudo ln -s /usr/local/lib/mmitss/libasn.so.1.0 /usr/local/lib/mmitss/libasn.so
-sudo ln -s /usr/local/lib/mmitss/libdsrc.so.1.0 /usr/local/lib/mmitss/libdsrc.so
+ln -s /usr/local/lib/mmitss/libnetsnmp.so.35.0.0 /usr/local/lib/mmitss/libnetsnmp.so.35
+ln -s /usr/local/lib/mmitss/libglpk.so.40.3.0 /usr/local/lib/mmitss/libglpk.so.40
+ln -s /usr/local/lib/mmitss/liblocAware.so.1.0 /usr/local/lib/mmitss/liblocAware.so
+ln -s /usr/local/lib/mmitss/libasn.so.1.0 /usr/local/lib/mmitss/libasn.so
+ln -s /usr/local/lib/mmitss/libdsrc.so.1.0 /usr/local/lib/mmitss/libdsrc.so
 fi
 
 sleep 1s
 
-sudo ldconfig
+ldconfig
 
 sleep 2s
 echo "Added required environment variables in ~/.bashrc file."
