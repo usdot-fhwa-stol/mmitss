40390a0be520e50228044f3fdb81b72a931b160d
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2025-04-21 13:00:36 -0400

    Update carma_time_lib.sh

--- mmitss/build/dockerfiles/x86/scripts/carma_time_lib.sh
+++ mmitss/build/dockerfiles/x86/scripts/carma_time_lib.sh
@@ -2,7 +2,7 @@
 set -ex
 
 echo "deb [trusted=yes] http://s3.amazonaws.com/stol-apt-repository develop focal" > /etc/apt/sources.list.d/stol-apt-repository.list
-apt remove carma-clock-1 udp-socket-1 udp-time-sync
+apt purge carma-clock-1 udp-socket-1 udp-time-sync-1 -y > /dev/null
 
 apt-get update && apt-get install -y \
     curl \
