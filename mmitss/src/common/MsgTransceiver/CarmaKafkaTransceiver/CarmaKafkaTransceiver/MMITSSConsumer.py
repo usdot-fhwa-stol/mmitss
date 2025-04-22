3006cf425b9d7c8a23a0a9a13f70c3824bb7d6b0
dev <dev@Focal.myguest.virtualbox.org>
2025-04-22 13:10:31 -0400

    Fix Kafka Transceiver

--- mmitss/src/common/MsgTransceiver/CarmaKafkaTransceiver/CarmaKafkaTransceiver/MMITSSConsumer.py
+++ mmitss/src/common/MsgTransceiver/CarmaKafkaTransceiver/CarmaKafkaTransceiver/MMITSSConsumer.py
@@ -14,7 +14,6 @@ class MMITSSConsumer(Consumer):
         super().__init__(consumerConf)
         self.config = self.readSocketConfig(socketConfigFilename)
         self.subscribe(topics)
-        logging.basicConfig(level=logging.DEBUG)
 
     def readSocketConfig(self,filename=None):
         # Read a config file into a json object:
@@ -94,13 +93,17 @@ class MMITSSConsumer(Consumer):
             time_sync_ports.append(self.config["TimeSyncPort"]["SnmpEngine"])
 
             msg = self.decodeTimeSync(msg)
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            logging.debug("Opening socket for TimeSync")
+            s.bind((hostIp,port))
             for receivingPort in time_sync_ports:
                 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 logging.debug(f"Sending TimeSync message to port {receivingPort}")
-                s.bind((hostIp,port))
                 communicationInfo = (hostIp, receivingPort)
                 s.sendto(msg.encode(),communicationInfo)
-                s.close()
+            s.close()
+            logging.debug("Closed socket for TimeSync")
+
 
         if self.kind != "TimeSync":    
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
