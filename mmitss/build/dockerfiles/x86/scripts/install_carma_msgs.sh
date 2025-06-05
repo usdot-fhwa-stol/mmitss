7c5c4f3bc4a4e2561c6ee8a7d6da894e1d49edf3
Misheel Bayartsengel <misheel.bayart@gmail.com>
2025-06-05 11:37:38 -0400

    Update install_carma_msgs.sh

--- mmitss/build/dockerfiles/x86/scripts/install_carma_msgs.sh
+++ mmitss/build/dockerfiles/x86/scripts/install_carma_msgs.sh
@@ -21,6 +21,7 @@ apt update
 export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrastructure/ros-apt-source/releases/latest | grep -F "tag_name" | awk -F'"' '{print $4}')
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb"
 apt install -y /tmp/ros2-apt-source.deb
+# Clean the installation deb file
 rm /tmp/ros2-apt-source.deb
 
 # Install ROS 2 packages
